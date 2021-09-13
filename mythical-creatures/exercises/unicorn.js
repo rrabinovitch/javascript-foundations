b4dd41f905409f6ba81a761b48964b414ee21006
Ruthie Rabinovitch <ruthie.rabinovitch@ibotta.com>
2021-09-13 10:22:21 -0600

    Complete unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,18 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+
+  says(words) {
+    return `**;* ${words} *;**`;
+  };
 }
 
 module.exports = Unicorn;
