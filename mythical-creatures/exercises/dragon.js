8f42e3c3acf048371740cfcb9ecbf4791f9330ce
Ruthie Rabinovitch <ruthie.rabinovitch@ibotta.com>
2021-09-22 11:42:39 -0600

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+    constructor(name, rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.meals = 0;
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`;
+    }
+
+    eat() {
+        this.meals += 1;
+        if (this.meals >= 3) {
+            this.hungry = false;
+        };
+    }
+}
+
+module.exports = Dragon;
